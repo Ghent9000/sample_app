3f42dea90379604f04eb2e874e1d56194e155132
Ghent9000 <jonaske_@hotmail.com>
2013-01-28 17:06:56 +0100

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -20,6 +20,10 @@ describe User do
 
   subject { @user }
 
+  it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
+  it { should respond_to(:authenticate) }  
+
   it { should respond_to(:name) }
   it { should respond_to(:email) }
   it { should respond_to(:password_digest) }
@@ -109,4 +113,9 @@ describe User do
     end
     it { should_not be_valid }
   end
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  end
+
 end
